675639bbaa17338a4f8821de537ff046cce6e687
Minjeong <minjeong1642@gmail.com>
2018-01-24 23:46:25 +0900

    Update download.sh

--- download.sh
+++ download.sh
@@ -7,7 +7,7 @@ tar -xzvf training-parallel-europarl-v7.tgz
 rm training-parallel-europarl-v7.tgz
 
 wget http://statmt.org/wmt14/training-parallel-nc-v9.tgz
-tar -xzvf training-parallel-europarl-v7.tgz
-rm training-parallel-europarl-v7.tgz
+tar -xzvf training-parallel-nc-v9.tgz
+rm training-parallel-nc-v9.tgz
 
 cd ..
